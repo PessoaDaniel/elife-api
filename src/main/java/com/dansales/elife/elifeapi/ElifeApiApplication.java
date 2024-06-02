a4d96a10c23c72cb2dfd0d3a20170c1a01c798a9
Daniel <danielpessoa432@gmail.com>
2024-06-02 13:35:11 -0300

    initial-commit

---
+++ src/main/java/com/dansales/elife/elifeapi/ElifeApiApplication.java
@@ -0,0 +1,13 @@
+package com.dansales.elife.elifeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElifeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElifeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dansales/elife/elifeapi/ElifeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dansales.elife.elifeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElifeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
